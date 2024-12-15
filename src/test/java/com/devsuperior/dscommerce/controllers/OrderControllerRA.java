84f2e4bbf3350c3322679c4e6a1c2fe2db63f92b
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-14 21:17:25 -0300

    Teste consulta pedidos por id outros cenarios

--- src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
@@ -59,5 +59,79 @@ public class OrderControllerRA {
 
 	}
 	
+	
+	@Test
+	public void findByIdShouldReturnOrderWhenIdExistsAndClientLogged() {
+		 given()
+	        .header("Content-type", "application/json")
+	        .header("Authorization", "Bearer " + clientToken)
+	        .accept(ContentType.JSON)
+	    .when()
+	        .get("/orders/{id}",existingOrderId)
+	    .then()
+	        .log().ifError() // Loga resposta em caso de erro
+	        .statusCode(200) // Espera que seja 200
+	        .body("id", is(1))
+	        .body("moment", equalTo("2022-07-25T13:00:00Z"))
+	        .body("status", equalTo("PAID"))
+	        .body("client.name", equalTo("Maria Brown"))
+	        .body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
+	        .body("items.name",hasItems("The Lord of the Rings","Macbook Pro"))
+	        .body("total",is(1431.0F));
+	        
+
+	}
+	
+	
+	@Test
+	public void findByIdShouldReturnForbiddenWhenIdExistsAndClientLoggedAndOrderDoesNotBelongUser() {
+		Long otherOrderId = 2L;
+		
+		 given()
+	        .header("Content-type", "application/json")
+	        .header("Authorization", "Bearer " + clientToken)
+	        .accept(ContentType.JSON)
+	    .when()
+	        .get("/orders/{id}",otherOrderId)
+	    .then()
+	        .log().ifError() // Loga resposta em caso de erro
+	        .statusCode(403); // Espera que seja 403
+	      
+	        
+
+	}
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndClientLogged() {
+		 given()
+	        .header("Content-type", "application/json")
+	        .header("Authorization", "Bearer " + adminToken)
+	        .accept(ContentType.JSON)
+	    .when()
+	        .get("/orders/{id}",nonExistingOrderId)
+	    .then()
+	        .log().ifError() // Loga resposta em caso de erro
+	        .statusCode(404); // Espera que seja 404
+	        
+	        
+
+	}
+	
+	
+	@Test
+	public void findByIdShouldReturnUnauthorizedWhenIdExistsAndInvalidToken() {
+		 given()
+	        .header("Content-type", "application/json")
+	        .header("Authorization", "Bearer " + invalidToken)
+	        .accept(ContentType.JSON)
+	    .when()
+	        .get("/orders/{id}",existingOrderId)
+	    .then()
+	        .log().ifError() // Loga resposta em caso de erro
+	        .statusCode(401); // Espera que seja 401
+	        
+	        
+
+	}
+	
 
 }
